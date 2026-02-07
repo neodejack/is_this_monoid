dcaa6ed5e0c6b8ca70af85707aae3fd7587beeb2
zili <neodejack@gmail.com>
2026-02-07 11:30:12 +0800

    generic fold

--- lib/predicates.ex
+++ lib/predicates.ex
@@ -1,33 +1,25 @@
 defmodule Predicates do
   @moduledoc """
+  a few predicates function
+  and a fold operation that combines these predicates function into a single function
   """
 
-  @doc """
-  Check if the argument can pass through any one of the predicates that we have defined
-
-  ## Examples
-
-      iex> Predicates.pass?("A")
-      false
-
-  """
-  def pass?(char) do
-    predicates = [&lowercase?/1, &digit?/1]
-    predicate = fold(predicates)
-    predicate.(char)
-  end
-
-  defp fold(predicates) when is_list(predicates) do
+  @spec fold([function()], function()) :: (String.t() -> boolean())
+  def fold(predicates, enum_func) when is_list(predicates) do
     fn char ->
-      Enum.any?(predicates, fn predicate -> predicate.(char) end)
+      enum_func.(predicates, fn predicate -> predicate.(char) end)
     end
   end
 
-  defp lowercase?(<<char::utf8>>) do
+  def lowercase?(<<char::utf8>>) do
     char in ?a..?z
   end
 
-  defp digit?(<<char::utf8>>) do
+  def uppercase?(<<char::utf8>>) do
+    char in ?A..?Z
+  end
+
+  def digit?(<<char::utf8>>) do
     char in ?0..?9
   end
 end
--- test/predicates_test.exs
+++ test/predicates_test.exs
@@ -1,20 +1,34 @@
 defmodule PredicatesTest do
   use ExUnit.Case
+  import Predicates
   doctest Predicates
 
+  def base_predicate() do
+    fold([&lowercase?/1, &digit?/1], &Enum.any?/2)
+  end
+
   test "char `a` should pass" do
-    assert Predicates.pass?("a") == true
+    assert base_predicate().("a") == true
   end
 
   test "char `7` should  pass" do
-    assert Predicates.pass?("7") == true
+    assert base_predicate().("7") == true
   end
 
   test "char `,` should not pass" do
-    assert Predicates.pass?(",") == false
+    assert base_predicate().(",") == false
   end
 
   test "char `A` should not pass" do
-    assert Predicates.pass?("A") == false
+    assert base_predicate().("A") == false
+  end
+
+  test "composing predicates" do
+    assert false == base_predicate() |> then(& &1.("A"))
+
+    assert true ==
+             base_predicate()
+             |> then(fn base -> fold([(&uppercase?/1) | base], &Enum.any?/2) end)
+             |> then(& &1.("A"))
   end
 end
